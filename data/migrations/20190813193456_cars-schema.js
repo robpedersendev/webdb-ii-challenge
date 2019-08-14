9ea2a8693a7446a35c0f9d5fc04a81b6b0ee0f94
robpedersendev <rob84044@gmail.com>
2019-08-13 19:37:46 -0600

    Set up the migrations file

--- data/migrations/20190813193456_cars-schema.js
+++ data/migrations/20190813193456_cars-schema.js
@@ -1,8 +1,19 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+      tbl.increments('id');
+      tbl
+        .varchar('Vin', 17)
+        .unique()
+        .notNullable();
+      tbl.text('Make').notNullable();
+      tbl.varchar('Model').notNullable();
+      tbl.integer('Mileage').notNullable();
+      tbl.varchar('Transmission');
+      tbl.varchar('Title');
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+  };
   
-};
